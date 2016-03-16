968fc8643db74c8a78d123f83b0d941930a0d781
Janelle Zorvan <jzorvan@gmail.com>
2016-03-16 09:24:07 -0500

    Both JSON files are now adding

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -2,27 +2,37 @@ function executeThisIfXHRFails(xhrEvent) {
   console.log("An error occured while transferring the data");
 }
 
-function codeRetriever() {
-  
-  var data = JSON.parse(this.responseText);  
-    
-  var productArea = document.getElementById("product-area");
-  
-  var outputString = "";    
-    
-  for (currentProduct in data.products) {
-    var outputString = "";
-    var product = data.products[currentProduct]
-    outputString += `<article`;
-    outputString += `<p id="name">${product.name}</p>`;
-    outputString += `<p id="price">${product.price}</p>`;
-    productArea.innerHTML += outputString;
+function productsRetriever() {
+
+ var productData = JSON.parse(this.responseText); 
+   categoriesRetriever(productData)
+
+
+  function categoriesRetriever() {
+
+    var categoriesRequest = new XMLHttpRequest;    
+
+    categoriesRequest.open("GET", "categories.json")
+    categoriesRequest.send();
+
+    categoriesRequest.addEventListener("load", categoriesParse);
+    categoriesRequest.addEventListener("error", executeThisIfXHRFails);
+
+  };
+
+  function categoriesParse() {
+            var categoryData = JSON.parse(this.responseText);
+            console.log("categoryData", categoryData);    
+            console.log("productData", productData);
+                    
   };
 };
+
+
     
 var productsRequest = new XMLHttpRequest();
 
-  productsRequest.addEventListener("load", codeRetriever);
+  productsRequest.addEventListener("load", productsRetriever);
   productsRequest.addEventListener("error", executeThisIfXHRFails);
 
   productsRequest.open("GET", "products.json");
